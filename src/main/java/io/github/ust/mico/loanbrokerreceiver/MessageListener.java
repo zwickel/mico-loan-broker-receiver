865453ce0293817b249eee614dd1a3cac17a4c2c
ma <mariusaltmann@outlook.de>
2021-05-25 17:26:41 +0200

    change format of message to string

--- src/main/java/io/github/ust/mico/loanbrokerreceiver/MessageListener.java
+++ src/main/java/io/github/ust/mico/loanbrokerreceiver/MessageListener.java
@@ -1,15 +1,11 @@
 package io.github.ust.mico.loanbrokerreceiver;
 
-import lombok.extern.slf4j.Slf4j;
-
-import com.fasterxml.jackson.databind.JsonNode;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Component;
 
-import io.github.ust.mico.loanbrokerreceiver.kafka.MicoCloudEventImpl;
+import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Component
@@ -27,7 +23,7 @@ public class MessageListener {
      * @param message
      */
     @KafkaListener(topics = "${kafka.input-topic}", groupId = "${kafka.group-id}")
-    public void receive(MicoCloudEventImpl<JsonNode> cloudEvent) {
+    public void receive(String cloudEvent) {
         log.debug("Received CloudEvent message: {}", cloudEvent);
         websocketsTemplate.convertAndSend("/topic/messaging-bridge", cloudEvent);
     }
